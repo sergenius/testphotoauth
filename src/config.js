22bb6af0a80deff300dd6caea98cba7ac8d3bf65
sergenius <engsergiobeltran@gmail.com>
2021-01-24 10:30:39 -0500

    change config

--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@ export default {
     MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "us-east-1",
-      BUCKET: "octank-ml-auth-uploads"
+      BUCKET: "octank-ml-auth-uploads-serbel"
     },
     cognito: {
       REGION: "us-east-1",
       USER_POOL_ID: "us-east-1_oHu2OVRvW",
-      APP_CLIENT_ID: "25qto3vujhsd1vfjvav5p3l1042fu2a5k5ehumcg3k7qu6dlp6f0",
-      IDENTITY_POOL_ID: "us-east-1:50d404fb-d0a5-4cad-91b5-7b08e8c54b89"
+      APP_CLIENT_ID: "25qto3vujhsd1vfjvav5p3l104",
+      IDENTITY_POOL_ID: "us-east-1:d1682880-cd37-4061-b481-f86e9de77815"
     },
     apiGateway: {
       REGION: "us-east-1",
@@ -16,6 +16,6 @@ export default {
       API_NAME: "SecurityAIAPI"
     },
     social: {
-      FB: "2002492656528231"
+      FB: "2002492656520412"
     }
   };
