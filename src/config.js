397e92ac838edf4f83e1613250e14a8e5cd04960
sergenius <engsergiobeltran@gmail.com>
2021-01-24 21:31:14 -0500

    testprod22

--- src/config.js
+++ src/config.js
@@ -1,21 +1,21 @@
 export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "octank-ml-auth-uploads-serbel"
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_oHu2OVRvW",
-      APP_CLIENT_ID: "25qto3vujhsd1vfjvav5p3l104",
-      IDENTITY_POOL_ID: "us-east-1:d1682880-cd37-4061-b481-f86e9de77815"
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://xv6f3udio0.execute-api.us-east-1.amazonaws.com/dev",
-      API_NAME: "SecurityAIAPI"
-    },
-    social: {
-      FB: "2002492656520412"
-    }
-  };
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "test6sup"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_oDI6ApOko",
+    APP_CLIENT_ID: "37cb136tceft505ppft852gvh9",
+    IDENTITY_POOL_ID: "us-east-1:76824bd6-2b0c-4280-b1e2-726a0ddc04c9"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://xv6f3udio0.execute-api.us-east-1.amazonaws.com/dev",
+    API_NAME: "SecurityAIAPI"
+  },
+  social: {
+    FB: "2002492656520412"
+  }
+};
